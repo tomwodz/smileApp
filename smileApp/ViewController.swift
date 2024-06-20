29afcbf18b14556d6ff2f4119f7146cd8f7eee8e
tomwodz <twodzinski@gmail.com>
2024-06-20 22:46:55 +0200

    Initial Commit

---
+++ smileApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  smileApp
+//
+//  Created by Mini-M1 on 20/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
